7f86a657e9634c1064c3aa8f28164338754ba689
vlfedotov <fedotov.vu@gmail.com>
2017-10-03 21:14:39 +0300

    auth base component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 
 
 export default class Auth extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            token: '',
+            name: ''
+        };
+    }
+
+
+
+
+
     render() {
         return (
-            <div>Auth</div>
+            <div className="container-fluid">
+                <form>
+                    <div className="form-group">
+                        <label>Email address
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label>Password
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" value="Check me out" /> Subscribe to newsletter?
+                        </label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         );
     }
 }
