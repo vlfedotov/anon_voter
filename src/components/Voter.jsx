7c346bf9c044b8719090f852e11f56ecf0842c91
vlfedotov <fedotov@gmail.com>
2017-10-06 08:51:58 +0300

    add getCriterion, getRole, setResult to Voter

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import { db } from '../config.jsx';
+
+console.log(db);
 
 function OneVote(props) {
     return <div>
@@ -21,25 +24,91 @@ export default class Voter extends Component {
 
         this.vote = this.vote.bind(this);
         this.getProperCriterion = this.getProperCriterion.bind(this);
+        this.getProperApplicants = this.getProperApplicants.bind(this);
 
         this.state = {
             user: props.user,
-            applicants: []
+            role: '',
+            criterion: {},
+            applicants: [],
+            
+            current_criteria: 'tests',
+            winners: []
         };
 
-        this.state['criterion'] =this.getProperCriterion();
+        this.getProperCriterion(props.user);
+        this.getProperApplicants(props.user);
+        
     }
 
-    getProperCriterion() {
+    getProperApplicants(user) {
+        db.getRole(user)
+            .then(snap => {
+                const user_role = snap.val()[user].role;            
+                db.getColleges(user_role)
+                    .then(snap => {
+                        const dirty_applicants = snap.val();
+                        let clean_applicants = {};
+                        for (var key in dirty_applicants) {
+                            let ap = dirty_applicants[key];
+                            console.log(ap.joined);
+                            let dirty_joined = ap.joined.split(".");
+                            let joined = new Date(dirty_joined[2], dirty_joined[1] - 1, dirty_joined[0]);
+                            const today = new Date();
+                            let elapsed = (today - joined) / (1000 * 60 * 60 * 24);
+                            if (key !== user && ap.role === user_role && elapsed > (90 + 30)) {
+                                clean_applicants[key] = ap.name;
+                            }
+                        }
+                        this.setState({applicants: clean_applicants});
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-        return [this.state.user, this.state.user]
+    getProperCriterion(user) {
+        db.getRole(user)
+            .then(snap => {
+                const user_role = snap.val()[user].role;            
+                this.setState({'role': user_role});
+                this.vote();
+                console.log(user_role);
+                db.getCriterion(user_role)
+                    .then(snap => {
+                        this.setState({'criterion': snap.val()[user_role]});
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     vote() {
-        const email = this.state.email;
-        const pass = this.state.password;
-    }
+        const MONTHS = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"
+                       ];
+                    
+        const today = new Date(),
+              year = today.getUTCFullYear(),
+              month = MONTHS[today.getMonth()];
 
+        const ref = db.setResult(year, month, this.state.user, this.state.role);
+        console.log(ref);
+        const updates = {
+            [this.state.current_criteria]: ['zx', 'as']
+        };
+
+        ref.update(updates);
+    }
 
     render() {
         return (
--- src/config.jsx
+++ src/config.jsx
@@ -9,4 +9,37 @@ const app = firebase.initializeApp({
     messagingSenderId: "401962735618"
 });
 
+const db = {
+    db: app.database(),
+
+    setResult(year, month, user, role) {
+        console.log('set Result for ' + year + ' ' + month + ' ' + role);
+        const ref = this.db.ref('results/' + year + '/' + month + '/' + role + '/history/' + user);
+        //const a = ref.once('value');
+        return ref;
+    },  
+    
+    getRole(user) {
+        console.log('get Role for ' + user);
+        const ref = this.db.ref('users');
+        const a = ref.once('value');
+        return a;
+    },
+
+    getColleges(role) {
+        console.log('get Colleges for ' + role);
+        const ref = this.db.ref('users');
+        const a = ref.once('value');
+        return a;
+    },
+    
+    getCriterion(role) {
+        console.log('get Criterion for ' + role);
+        const ref = this.db.ref('criterion');
+        const a = ref.once('value');
+        return a;
+    }
+};
+
+export { db };
 export default app;
